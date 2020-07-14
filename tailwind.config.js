896bf816bf59d0532a4e3efce011579a2554e7a7
AxSch <alex_schuneman@outlook.com>
2020-07-14 19:33:31 +0100

    Add custom tailwind classes

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontSize: {
+                '7xl': '5rem',
+                '8xl': '6rem',
+                'landing-lg': '15.875rem',
+            },
+            fontFamily: {
+                'serif': ['Holtwood One SC'],
+                'sans-serif': ['Open Sans'],
+                'sans': ['Montserrat Subrayada'],
+                'san': ['Kanit'],
+            },
+            margin: {
+                '15': '15rem'
+            },
+            width: {
+                '500': '500px',
+                '552': '552px'
+            },
+            height: {
+                '500': '500px'
+            },
+            inset: {
+                '1/3': '33%',
+                '1/4': '66%',
+                '38': '38%',
+                '-14': '-14%',
+                '-30': '-30%',
+                '-50': '-50%',
+            },
+            padding: {
+                '50': '50%',
+                '53': '53%',
+            }
+        }
+    },
+}
